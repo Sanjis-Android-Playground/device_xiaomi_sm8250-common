f3faa5156a116b542237a39b5c6fbadfe0873bc9
althafvly <althafvly@gmail.com>
2023-06-20 11:12:20 +0530

    sm8250-common: Build fastcharge implementation
    Change-Id: I3e6994d2c2f419dc8ce98a060204fda3dd16df81
    Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

--- kona.mk
+++ kona.mk
@@ -214,6 +214,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fastcharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.xiaomi_sm8250
+
 # Fingerprint
 ifneq ($(TARGET_IS_TABLET),true)
 PRODUCT_PACKAGES += \
